666e3e918db08b127208716a1b9903c4285c0544
Yves <73966483+Yvestxt@users.noreply.github.com>
2020-11-16 16:54:52 -0300

    Update Dashboardbak.py

--- Dashboardbak.py
+++ Dashboardbak.py
@@ -1,4 +1,3 @@
-#Yves & Maria Clara & Marina Cristina & Eduardo
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -6,10 +5,14 @@ import plotly.express as px
 import pandas as pd
 import plotly.graph_objects as go
 
-
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 pets = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+#cores
+colors = {
+    'background': '#bbf6f6',
+}
 
 #distribuição de dados
 #dados abandono
@@ -29,21 +32,35 @@ total = pd.DataFrame({
     "Média": [655800000, 360800000, 271900000, 205200000, 70300000, 1500000000, 649793940, 483116188, 381050666, 220866000, 80628340, 1594810000],
     "Anos": ["Ano de 2013", "Ano de 2013", "Ano de 2013", "Ano de 2013", "Ano de 2013", "Ano de 2013","Ano de 2018" , "Ano de 2018" ,"Ano de 2018" ,"Ano de 2018" ,"Ano de 2018" ,"Ano de 2018"]
 })
-#dados Louyse & Pedro
-pizzacaes =go.Figure(go.Sunburst(
+#dados estado
+estados = pd.DataFrame({
+    "ESTADOS": ['Acre', 'Alagoas','Amapá','Amazonas','Bahia','Ceará','Distrito Federal','Espírito Santo','Goiás','Maranhão','Mato Grosso','Mato Grosso Sul','Minas Gerais','Pará','Paraíba','Paraná','Pernambuco','Piauí','Rio de Janeiro','Rio Grande do Norte','Rio Grande do Sul','Rondônia','Roraima','Santa Catarina','São Paulo','Sergipe','Tocantins','Acre', 'Alagoas','Amapá','Amazonas','Bahia','Ceará','Distrito Federal','Espírito Santo','Goiás','Maranhão','Mato Grosso','Mato Grosso Sul','Minas Gerais','Pará','Paraíba','Paraná','Pernambuco','Piauí','Rio de Janeiro','Rio Grande do Norte','Rio Grande do Sul','Rondônia','Roraima','Santa Catarina','São Paulo','Sergipe','Tocantins'],
+    "VALORES": [120, 323, 81, 404, 1691, 944, 293, 494, 1115, 794, 605, 457, 3256, 1045, 450, 2183, 947, 445, 2117, 375, 2384, 305, 64, 1256, 6289, 240, 183, 53, 216, 42, 171, 1016, 752, 62, 143, 268, 577, 212, 168, 1019, 493, 262, 597, 537, 323, 748, 194, 920, 149, 29, 369, 1950, 137, 106],
+    "RAÇA": ["Cães","Cães","Cães","Cães","Cães","Cães","Cães","Cães","Cães","Cães","Cães","Cães","Cães","Cães","Cães","Cães","Cães","Cães","Cães","Cães","Cães","Cães","Cães","Cães","Cães","Cães","Cães","Gatos","Gatos","Gatos","Gatos","Gatos","Gatos","Gatos","Gatos","Gatos","Gatos","Gatos","Gatos","Gatos","Gatos","Gatos","Gatos","Gatos","Gatos","Gatos","Gatos","Gatos","Gatos","Gatos","Gatos","Gatos","Gatos","Gatos"]
+})
+#dados pizza/motivos(Cães)
+pizzacaes = go.Figure(go.Sunburst(
     labels=["Segurança/Guarda", "Companhia", "Diversão/Afetividade", "Outros", "Até 5 salários", "Mais de 5 a 10 salários", "Mais de 10 salários", "Sem declaração", "Até 5 salários", "Mais de 5 a 10 salários", "Mais de 10 salários", "Sem declaração", "Até 5 salários", "Mais de 5 a 10 salários", "Mais de 10 salários", "Sem declaração", "Até 5 salários", "Mais de 5 a 10 salários", "Mais de 10 salários", "Sem declaração"],
     parents=["Cães", "Cães", "Cães", "Cães", "Segurança/Guarda", "Segurança/Guarda", "Segurança/Guarda", "Segurança/Guarda", "Companhia", "Companhia", "Companhia", "Companhia", "Diversão/Afetividade", "Diversão/Afetividade", "Diversão/Afetividade", "Diversão/Afetividade", "Outros", "Outros", "Outros", "Outros"],
-    values=[144, 429, 416, 17, 140, 647, 724, 99, 163, 429, 617, 94, 57, 206, 209, 24],
+    values=[1006, 1610, 1303, 496, 144, 429, 416, 17, 140, 647, 724, 99, 163, 429, 617, 94, 57, 206, 209, 24],
+    branchvalues = "total",
 ))
-
+#dados pizza/motivos(Gatos)
+pizzagatos = go.Figure(go.Sunburst(
+    labels=["Segurança/Guarda", "Companhia", "Diversão/Afetividade", "Outros", "Até 5 salários", "Mais de 5 a 10 salários", "Mais de 10 salários", "Sem declaração", "Até 5 salários", "Mais de 5 a 10 salários", "Mais de 10 salários", "Sem declaração", "Até 5 salários", "Mais de 5 a 10 salários", "Mais de 10 salários", "Sem declaração", "Até 5 salários", "Mais de 5 a 10 salários", "Mais de 10 salários", "Sem declaração"],
+    parents=["Gatos", "Gatos", "Gatos", "Gatos", "Segurança/Guarda", "Segurança/Guarda", "Segurança/Guarda", "Segurança/Guarda", "Companhia", "Companhia", "Companhia", "Companhia", "Diversão/Afetividade", "Diversão/Afetividade", "Diversão/Afetividade", "Diversão/Afetividade", "Outros", "Outros", "Outros", "Outros"],
+    values=[313, 291, 332, 59, 0, 97, 194, 22, 8, 66, 208, 9, 22, 92, 210, 8, 0, 2, 57, 0],
+    branchvalues = "total",
+))
 #color_discrete_map={"Cães":"goldenrod","Gatos":"magenta"})
-#processamento de dados
 
+#processamento de dados
 abandfig = px.bar(abandono, x="Causas", y="Média", color="Pets", barmode="group")
 medfig = px.bar(media, x="Pet", y="Gasto mensal", barmode="group")
 totalfig = px.bar(total, x="Espécie dos PETs", y="Média", color="Anos", barmode="group")
 pizzacaes.update_layout(margin = dict(t=0, l=0, r=0, b=0))
-
+pizzagatos.update_layout(margin = dict(t=0, l=0, r=0, b=0))
+estadofig = px.bar(estados, x="ESTADOS", y="VALORES", color="RAÇA", barmode="group")
 
 #layout
 pets.layout = html.Div(children=[
@@ -83,6 +100,16 @@ pets.layout = html.Div(children=[
         figure=totalfig
     ),
 #fim do terceiro gráfico (Renann & Hugo)
+    html.H1(children='Relação gatos e cães a cada mil domicilios',
+    style={
+        'textAlign': 'center'
+    }
+),
+    dcc.Graph(
+        id='estadofig',
+        figure=estadofig
+    ),
+#fim do quarto gráfico (Mayara & Maria Cláudia)
     html.H1(children='Cães x Gatos por salários mínimos',
     style={
             'textAlign': 'center'
@@ -92,9 +119,13 @@ pets.layout = html.Div(children=[
         id='pizzacaes',
         figure=pizzacaes
     ),
+    dcc.Graph(aside={
+        id='pizzagatos',
+        figure=pizzagatos
+    }
+    ),
 ])
 
-    
 #player
 if __name__ == '__main__':
     pets.run_server(debug=True)
